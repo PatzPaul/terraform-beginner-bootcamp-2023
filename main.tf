0316f202308d02ae948edc8d9ca12f0720616e5d
PatzPaul <82320162+PatzPaul@users.noreply.github.com>
2023-10-08 22:57:00 +0300

    #24 Bucket should no longer use random provider (#25)

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
 
-resource "random_string" "bucket_name" {
-  lower   = true
-  upper   = false
-  length  = 32
-  special = false
-}
+resource "aws_s3_bucket" "website_bucket" {
+
+  bucket = var.bucket_name
 
 
-resource "aws_s3_bucket" "example" {
-
-  bucket = random_string.bucket_name.result
-
-
-   tags = {
+  tags = {
     UserUuid = var.user_uuid
-   }
-   }
+  }
+}
 
