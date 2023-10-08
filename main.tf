4f4142594e9a832ca70c2b938fc5be7cf9ad7cba
PatzPaul <pathefister@gmail.com>
2023-10-08 20:38:59 +0000

    #26 Refractor S3 bucket into the new terrahouse_aws module

--- main.tf
+++ main.tf
@@ -1,11 +1,23 @@
+terraform {
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
 
-resource "aws_s3_bucket" "website_bucket" {
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  #cloud {
+  #  organization = "ExamPro"
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
 
-  bucket = var.bucket_name
-
-
-  tags = {
-    UserUuid = var.user_uuid
-  }
 }
 
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+}
